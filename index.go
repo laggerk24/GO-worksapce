7efdb6e99b5f9fcb5d09e09843fa1332975b1dac
laggerk24 <laggerk24@gmail.com>
2025-08-16 19:26:46 +0530

    go practice

---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var fruit = "Mango"
+var Rawfruit string = "Raw Mango"
+
+func main() {
+	ripeFruit := "Ripe Fruit"
+	fmt.Println(fruit)
+	fmt.Println(ripeFruit)
+	fmt.Println(Rawfruit)
+	fmt.Println("Hello World")
+}
