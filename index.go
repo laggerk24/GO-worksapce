35d7ee3f0e82413bc408e165beb7590266985555
laggerk24 <laggerk24@gmail.com>
2025-08-18 23:10:28 +0530

    math operations

--- index.go
+++ index.go
@@ -7,10 +7,18 @@ import (
 var fruit = "Mango"
 var Rawfruit string = "Raw Mango"
 
+const a = 20
+
 func main() {
 	ripeFruit := "Ripe Fruit"
+	fmt.Println(Rawfruit)
+	Rawfruit = ripeFruit
 	fmt.Println(fruit)
 	fmt.Println(ripeFruit)
 	fmt.Println(Rawfruit)
 	fmt.Println("Hello World")
+	fmt.Println(20-2)
+	fmt.Println(2+2)
+	// a = 30
+	fmt.Println(a)
 }
